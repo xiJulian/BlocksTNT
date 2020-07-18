2b3d5cdf10edbc1dbe598c6505d95d5d76c4db3b
Julian <julian.dow67@icloud.com>
2020-07-18 20:22:16 +0300

    Delete Server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-const bot = require('./Bot/index.js');
-// const server = require('./Server/index.js');
-// server(bot);
+const mineflayer = require('mineflayer');
+const { pathfinder } = require('mineflayer-pathfinder');
+const fs = require('fs');
+
+const bot = mineflayer.createBot({
+    username: 'xiJulian___',
+    host: 'blocksmc.com',
+    version: '1.12.2',
+    plugins: [pathfinder]
+});
+
+fs.readdir('./src/listeners/', (err, files) => {
+    if (err) console.error(err);
+    else files.forEach(file => {
+        let event = require('./listeners/' + file);
+        bot.on(file.split('.')[0], (...params) => event(bot, ...params));
+    });
+});
 
 process.on('unhandledRejection', error => {
-    console.error(error.stacl || error);
+    console.error(error.stack || error);
 });
